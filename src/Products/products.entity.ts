33c38365acf7be1f9fae2a164edb2747fe959302
plumest <plumest.chayathon@gmail.com>
2022-07-06 17:30:49 +0700

    Change id's types

--- src/Products/products.entity.ts
+++ src/Products/products.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
 
   @Column()
   name: string;
