85cfc35e45353fb0c2178570362686625928c337
plumest <plumest.chayathon@gmail.com>
2022-07-06 16:27:02 +0700

    Connect Microsoft SQL ✌🏻

---
+++ src/Products/productsController.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  async getProducts() {
+    return await this.productsService.findAll();
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './Products/products.entity';
+import { ProductsModule } from './Products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'sa',
+      password: 'str0ng_secret',
+      database: 'ProductDB',
+      entities: [Product],
+      synchronize: true,
+      extra: {
+        trustServerCertificate: true,
+      },
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
